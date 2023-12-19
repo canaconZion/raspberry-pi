b782f9b47dacd03e41b038743d4d555afd0e08e5
canaconZion <3117188561@qq.com>
2023-12-19 22:50:11 +0800

    feat: get remote inffo

--- device_information.py
+++ device_information.py
@@ -1,8 +1,12 @@
-import uuid
 import socket
-import time
-import psutil
 import sys
+import time
+import uuid
+
+import psutil
+import os
+import platform
+
 
 def get_mac_address():
     mac=uuid.UUID(int = uuid.getnode()).hex[-12:].upper()
@@ -35,21 +39,58 @@ def get_cpu_memory_usage():
         while True:
             cpu_percent = psutil.cpu_percent(interval=1)
             memory_percent = psutil.virtual_memory().percent
+            local_time,weekday_string = get_time()
+            time_string = f"TIME: {local_time} {weekday_string} | "
 
             output_string = f"CPU: {cpu_percent:.2f}% | Memory: {memory_percent:.2f}%"
-            sys.stdout.write("\r" + output_string)
+            sys.stdout.write("\r" + time_string+ output_string)
             sys.stdout.flush()
             time.sleep(1)
     except KeyboardInterrupt:
         print("\n程序已终止.")
 
+def get_remote_ip():
+    remote_ip=""
+    remote_port=""
+    with open("/home/zion/workdir/frpc/frpc.toml", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if "serverAddr" in line:
+                equal_sign_index = line.find('=')
+                if equal_sign_index != -1:
+                    value = line[equal_sign_index + 1:].strip()
+                    remote_ip = value.replace("\"","")
+            elif "remotePort" in line:
+                equal_sign_index = line.find('=')
+                if equal_sign_index != -1:
+                    value = line[equal_sign_index + 1:].strip()
+                    remote_port = value
+    return remote_ip,remote_port
+
+def get_system_info():
+    system_info = os.uname()
+    memory_info = psutil.virtual_memory()
+    disk_info = psutil.disk_usage('/')
+
+    system_info = platform.system()
+    version_info = platform.version()
+    architecture_info = platform.architecture()
+
+
+
 if __name__ == '__main__':
     mac = get_mac_address()
     name = get_computer_name()
     ip = get_local_ip()
-    local_time,weekday_string = get_time()
+    
+    remote_ip,remote_port = get_remote_ip()
+    
     print("COMPUTER_NAME: ",name)
     print("IP: ",ip)
+    print(f"FRP: {remote_ip}:{remote_port}")
     print("MAC: ",mac)
-    print("TIME: ",local_time,weekday_string)
     get_cpu_memory_usage()
+    
+
+
+        
